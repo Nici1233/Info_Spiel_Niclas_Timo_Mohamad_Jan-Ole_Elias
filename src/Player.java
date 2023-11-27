6c91c8f004bceadd1ae0c33321f107d728a94dae
angrtimo06 <timo.angrick@web.de>
2023-11-27 12:27:12 +0100

    player move

--- src/Player.java
+++ src/Player.java
@@ -8,5 +8,49 @@ import java.util.List;
  */
 public class Player extends Actor {
 
+    public void act() {
+        performMovement();
+    }
+
+    private void performMovement() {
+        if (Greenfoot.isKeyDown("W")) {
+            turn(Direction.NORTH);
+            move();
+
+        }
+        if (Greenfoot.isKeyDown("A")) {
+            turn(Direction.WEST);
+            move();
+
+        }
+        if (Greenfoot.isKeyDown("S")) {
+            turn(Direction.SOUTH);
+            move();
+
+        }
+        if (Greenfoot.isKeyDown("D")) {
+            turn(Direction.EAST);
+        }
+    }
+
+    public void move() {
+        if (canMove()) {
+            move(1);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }

e91290ea2838db50342789af153c6c0b9bb1941d
dehnniclas06 <niclas.dehn@live.de>
2023-12-03 14:48:01 +0100

    new classes

---
+++ src/Basement.java
@@ -0,0 +1,26 @@
+import greenfoot.World;
+
+/**
+ * Write a description of class RabbitLevel1 here.
+ *
+ * @author SAE
+ * @version 1.0
+ */
+public class Basement extends MasterLevel
+{
+
+
+    /**
+     * Constructor for objects of class RabbitLevel1.
+     *
+     */
+    public Basement()
+    {
+
+        setBackground("./images/cell.jpg");
+        setPaintOrder(Player.class);
+        addObject(new Player(), 0,0);
+    }
+    //Test
+
+}

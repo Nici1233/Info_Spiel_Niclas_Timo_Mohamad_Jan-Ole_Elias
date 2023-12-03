089daf99e1c03c892f6657968976b9eba456d034
dehnniclas06 <niclas.dehn@live.de>
2023-12-03 14:48:01 +0100

    Plank10 Design

---
+++ src/MasterLevel.java
@@ -0,0 +1,26 @@
+import greenfoot.World;
+
+/**
+ * Write a description of class RabbitLevel1 here.
+ *
+ * @author SAE
+ * @version 1.0
+ */
+public class MasterLevel extends World
+{
+
+
+    /**
+     * Constructor for objects of class RabbitLevel1.
+     *
+     */
+    public MasterLevel()
+    {
+        super(8, 8, 60);
+        setBackground("./images/cell.jpg");
+        setPaintOrder(Player.class);
+
+    }
+    //Test
+
+}

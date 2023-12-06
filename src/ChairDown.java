975f4e2b411031d8c0142b64eb1e7cedfd49b0ec
timoa <timo.angrick@web.de>
2023-12-06 19:29:56 +0100

    Board

---
+++ src/ChairDown.java
@@ -0,0 +1,4 @@
+import greenfoot.Actor;
+
+public class ChairDown extends Actor {
+}
--- src/TestWorld.java
+++ src/TestWorld.java
@@ -16,11 +16,31 @@ public class TestWorld extends World
      */
     public TestWorld()
     {
-        super(20, 10, 60);
+        super(16, 8, 60);
         //setBackground("./images/cell.jpg");
         setBackground("./images/Boden_1.jpg");
         setPaintOrder(Player.class);
         addObject(new Player(), 0,0);
+
+        addObject(new Chair(),1,1);
+        addObject(new Chair(),1,2);
+        addObject(new Chair(),1,5);
+        addObject(new Chair(),1,6);
+        addObject(new Chair(),4,1);
+        addObject(new Chair(),4,2);
+        addObject(new Chair(),4,5);
+        addObject(new Chair(),4,6);
+        addObject(new Chair(),7,1);
+        addObject(new Chair(),7,2);
+        addObject(new Chair(),7,5);
+        addObject(new Chair(),7,6);
+        addObject(new Chair(),10,1);
+        addObject(new Chair(),10,2);
+        addObject(new Chair(),10,5);
+        addObject(new Chair(),10,6);
+        addObject(new ChairDown(),14,4);
+
+        addObject(new Board(),15,4);
     }
     
 }

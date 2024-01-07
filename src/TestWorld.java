ca016517fc734c327e92e2f2e8f793537522f600
timoa <timo.angrick@web.de>
2024-01-07 17:44:42 +0100

    Classroom update Teacher + Pickaxe image

--- src/TestWorld.java
+++ src/TestWorld.java
@@ -16,7 +16,7 @@ public class TestWorld extends World
      */
     public TestWorld()
     {
-        super(16, 8, 60);
+        super(14, 6, 60);
         //setBackground("./images/cell.jpg");
         setBackground("./images/Boden_1.jpg");
         setPaintOrder(Player.class);
@@ -25,35 +25,37 @@ public class TestWorld extends World
         addObject(new Chair(),1,1);
         addObject(new Chair(),1,2);
         addObject(new Chair(),1,5);
-        addObject(new Chair(),1,6);
+        addObject(new Chair(),1,4);
         addObject(new Chair(),4,1);
         addObject(new Chair(),4,2);
         addObject(new Chair(),4,5);
-        addObject(new Chair(),4,6);
+        addObject(new Chair(),4,4);
         addObject(new Chair(),7,1);
         addObject(new Chair(),7,2);
-        addObject(new Chair(),7,5);
-        addObject(new Chair(),7,6);
+        //addObject(new Chair(),7,5);
+        addObject(new Chair(),7,4);
         addObject(new Chair(),10,1);
         addObject(new Chair(),10,2);
         addObject(new Chair(),10,5);
-        addObject(new Chair(),10,6);
-        addObject(new ChairDown(),14,4);
+        addObject(new Chair(),10,4);
+        addObject(new ChairDown(),12,3);
 
-        addObject(new Board(),15,4);
+        addObject(new Board(),13,3);
 
-        addObject(new Door(),13,0);
+        addObject(new Door(),12,0);
 
         addObject(new Wall(),15,0);
         addObject(new Wall(),14,0);
 
-        addObject(new Key(), 14, 4);
+        addObject(new Key(), 12, 3);
 
 
 
 
 
-        for(int i=0; i<13; i++) {
+
+
+        for(int i=0; i<12; i++) {
             Wall wall = new Wall();
             addObject(wall, i, 0);
         }
